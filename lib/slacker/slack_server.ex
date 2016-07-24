41312c6d7a41862370cd1ee866ae4ba1f1c38a8f
Demarius Chrite <chrite.demarius@gmail.com>
2016-07-24 19:23:06 -0400

    Define initialization.

--- lib/slacker/slack_server.ex
+++ lib/slacker/slack_server.ex
@@ -4,4 +4,8 @@ defmodule Slacker.SlackServer do
   def start_link do
     GenServer.start_link(__MODULE__, self, name: __MODULE__)
   end
+
+  def init (pid) do
+    {:ok, slack_pid} = Slacker.Slack.start_link(Application.get_env(:slacker, :slack_token))
+  end
 end
