3342320dbad8fa7034ce2916eff6c1113661bc4e
Demarius Chrite <chrite.demarius@gmail.com>
2016-07-27 12:48:31 -0400

    Create a Supervisor.

--- lib/slacker.ex
+++ lib/slacker.ex
@@ -1,2 +1,17 @@
 defmodule Slacker do
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      worker(Slacker.SlackServer, [])
+    ]
+
+    opts = [
+      strategy: :one_for_one, name: Slacker.Supervisor
+    ]
+
+    Supervisor.start_link(children, opts)
+  end
 end
