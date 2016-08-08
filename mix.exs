48f5064344ca8481c23d1341dd0e44a5877ba689
Demarius Chrite <chrite.demarius@gmail.com>
2016-08-08 09:31:27 -0400

    Set version to `0.0.1`.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Slacker.Mixfile do
 
   def project do
     [app: :slacker,
-     version: "0.1.0",
+     version: "0.0.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
