de80bc43811a57bf60e2af28ffbb48ef11080287
Demarius Chrite <chrite.demarius@gmail.com>
2016-08-09 12:51:43 -0400

    Add slack to app config.

--- mix.exs
+++ mix.exs
@@ -14,7 +14,9 @@ defmodule Slacker.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [
+      :logger, :slack
+    ], mod: {}]
   end
 
   # Dependencies can be Hex packages:
